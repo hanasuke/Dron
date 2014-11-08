22e62dcb8fdba49754907080d83966ad61a36e7b
naosuke <naosuke@live.jp>
2014-11-09 01:55:36 +0900

    盤面クラスの定義

--- Board.java
+++ Board.java
@@ -1,3 +1,31 @@
+import java.util.Arrays;
+
 public class Board {
+  private int xSize, ySize;
+  private int[][] state;
+
+  Board (int difficulty) {
+    ySize = 80;
+    if ( difficulty == 1 ) {
+      xSize = 80;
+    } else if ( difficulty == 2 ) {
+      xSize = 120;
+    } else {
+      xSize = 160;
+    }
+    state = new int[ySize][xSize];
+    Arrays.fill(state, 0);
+    System.out.println(Arrays.toString(state));
+  }
+
+  public int getState(Point point) {
+    if ( point.x < 0 || point.x > xSize ) {
+      return -1;
+    } else if ( point.y < 0 || point.y > ySize ) {
+      return -1;
+    } else {
+      return state[point.y][point.x];
+    }
+  }
 
 }
