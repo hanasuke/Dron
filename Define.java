5f495360d6bd170ddb5a9bb0e2b3fecf5f26369c
Naoki Hanakawa <naosuke@live.jp>
2014-11-11 03:19:26 +0900

    移動方向を示す定数を定義

---
+++ Define.java
@@ -0,0 +1,7 @@
+public class Define {
+  // 移動方向の定数定義
+  public static final int LEFT  = 1;
+  public static final int UP    = 2;
+  public static final int RIGHT = 3;
+  public static final int DOWB  = 4;
+}
