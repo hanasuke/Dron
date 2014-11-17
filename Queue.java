093111c7d5ea753d13c8d350df5ee8bec74ffc52
Naoki Hanakawa <naosuke@live.jp>
2014-11-18 04:53:42 +0900

    トレーディングスペースの削除

--- Queue.java
+++ Queue.java
@@ -4,38 +4,38 @@ public class Queue {
   private Point t = new Point();
   private int head;
   private int tail;
-  
+
   Queue() {
     for ( int i = 0; i < SIZE+1; i++ ) {
       values[i] = new Point();
-      values[i].x = 0;       
+      values[i].x = 0;
       values[i].y = 0;
-    } 
+    }
     head = tail = 0;
   }
-  
+
   void init() {
     head = 0;
     tail = 0;
   }
-  
+
   Point watchhead() {
-    return values[head];   
+    return values[head];
   }
-  
+
   void enqueue(Point pos) {
     values[tail++] = pos;
     tail = tail % (SIZE+1);
   }
-  
+
   Point dequeue() {
-    t = values[head++];      
+    t = values[head++];
     head = head % (SIZE+1);
-    
+
     return t;
   }
-  
+
   boolean isEmpty() {
     return (tail == head);
   }
-} 
+}
